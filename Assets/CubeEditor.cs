bc2ad94a876fa579c36c85e50c13e554c53d2395
Stuart Gannon <stuart_gannon444@yahoo.co.uk>
2018-07-05 15:53:50 +0100

    Added numbers and text labels so we know where we are on our Grid

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnapping : MonoBehaviour {
+[SelectionBase]
+public class CubeEditor : MonoBehaviour {
 
     [SerializeField] [Range(1f,20f)] float gridSize = 10f;
 
-	void Update ()
+    TextMesh textMesh;
+
+    void Update ()
     {
         Snapping();
     }
@@ -16,9 +19,11 @@ public class EditorSnapping : MonoBehaviour {
     {
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
-        snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
 
-        transform.position = new Vector3(snapPos.x, snapPos.y, snapPos.z);
+        // Gets the first TextMesh it comes across in child objects
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
     }
 }
