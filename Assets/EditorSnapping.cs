65fa5511bc7419cb429d17eb53cb4cada7e9bbff
Stuart Gannon <stuart_gannon444@yahoo.co.uk>
2018-07-05 14:44:03 +0100

    Editor Snapping Implemented

---
+++ Assets/EditorSnapping.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnapping : MonoBehaviour {
+
+    [SerializeField] [Range(1f,20f)] float gridSize = 10f;
+
+	void Update ()
+    {
+        Snapping();
+    }
+
+    private void Snapping()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, snapPos.y, snapPos.z);
+    }
+}
