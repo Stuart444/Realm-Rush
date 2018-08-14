30bbccee043551fb2ab1a994132f680e8add7daa
Stuart Gannon <stuart_gannon444@yahoo.co.uk>
2018-08-14 15:01:02 +0100

    Towns looking at Enemies and Shooting them, yay.

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour {
+
+    [SerializeField] int hitPoints = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        print("I'm Hit");
+        
+        if(hitPoints <= 0)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void KillEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    void ProcessHit()
+    {
+        hitPoints--;
+        print("Current HP: " + hitPoints);
+    }
+}
